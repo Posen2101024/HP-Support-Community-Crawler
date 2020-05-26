2eb97a79c8e08195223a8799a40b9b42d005ab78
Posen <posen2101024@gmail.com>
2020-05-26 16:54:26 +0800

    change workdir

--- download/chromedriver.sh
+++ download/chromedriver.sh
@@ -5,22 +5,22 @@ else
 	version=${1}
 fi
 
-cd $(dirname "$0")/..
+cd $(dirname "$0")
 
-mkdir -p crawler/chromedriver/Linux &&\
+mkdir -p ../crawler/chromedriver/Linux &&\
 wget https://chromedriver.storage.googleapis.com/${version}/chromedriver_linux64.zip &&\
 unzip -o chromedriver_linux64.zip &&\
 rm chromedriver_linux64.zip &&\
-mv chromedriver crawler/chromedriver/Linux
+mv chromedriver ../crawler/chromedriver/Linux
 
-mkdir -p crawler/chromedriver/Mac &&\
+mkdir -p ../crawler/chromedriver/Mac &&\
 wget https://chromedriver.storage.googleapis.com/${version}/chromedriver_mac64.zip &&\
 unzip -o chromedriver_mac64.zip &&\
 rm chromedriver_mac64.zip &&\
-mv chromedriver crawler/chromedriver/Mac
+mv chromedriver ../crawler/chromedriver/Mac
 
-mkdir -p crawler/chromedriver/Windows &&\
+mkdir -p ../crawler/chromedriver/Windows &&\
 wget https://chromedriver.storage.googleapis.com/${version}/chromedriver_win32.zip &&\
 unzip -o chromedriver_win32.zip &&\
 rm chromedriver_win32.zip &&\
-mv chromedriver.exe crawler/chromedriver/Windows
+mv chromedriver.exe ../crawler/chromedriver/Windows
